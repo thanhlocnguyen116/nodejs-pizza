e088084368bcdbb26e4017f2b5f761e8ccb75c0a
Loc Nguyen <thanhlocnguyen116@gmail.com.vn>
2021-09-17 01:11:31 +0700

    fix controllers

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -3,7 +3,7 @@ function homeController() {
     return {
         async index(req, res) {
             const pizzas = await Menu.find()
-            return res.render('home', { pizzas: pizzas })
+            return res.render('home', { pizzas })
         }
     }
 }
